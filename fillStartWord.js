aaeec9c605a62532e94a88dac7c75bc3267ef66a
Ken <saharat.kanu@mail.kmutt.ac.th>
2022-09-08 22:45:46 +0700

    startWord

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,16 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  let concat = startWord+word
+  if(word == undefined || word == null){
+    return undefined;
+  }
+  else if(word.startsWith(startWord)){
+    return word;
+  }
+  else if(!word.startsWith(startWord)){ //startsWith
+    return concat;
+  }
+  
 }
 module.exports = fillStartWord
